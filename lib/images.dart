e54934b7eee534e9de160ac52a52b8209750eb56
MahmoudElSayed96 <gcfjxvkj@gmail.com>
2025-02-13 22:57:06 +0200

    Session 2 ( Expanded , Visibility , Wrap , Stack , Positioned , Icon, Image.asset , Image.network , ImageProvider )

---
+++ lib/images.dart
@@ -0,0 +1,11 @@
+class ImageManager {
+  static const String flutterBird = "images/photo1.png";
+  static const String jetPack = "images/photo2.png";
+  static const String android = "images/photo3.png";
+  static const String bloC = "images/photo4.png";
+
+  static const flutterNetworkImage =
+      "https://th.bing.com/th/id/R.4bdcacfe105931d059cc83aeae13133b?rik=um5ioxF5uolo%2bw&pid=ImgRaw&r=0";
+  static const flutterBirdNetwork =
+      "https://www.mindinventory.com/blog/wp-content/uploads/2022/10/flutter-3.png";
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_course_for_beginners_msc/images.dart';
 
 void main() {
   runApp(MyApp());
@@ -16,75 +17,302 @@ class MyApp extends StatelessWidget {
   }
 }
 
+// class HomeScreen extends StatelessWidget {
+//   const HomeScreen({super.key});
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//         body: SizedBox(
+//       width: double.infinity,
+//       height: double.infinity,
+//       child: Stack(
+//         alignment: Alignment.center,
+//         children: [
+//           //* green
+//           Positioned(
+//             top: 100,
+//             right: 20,
+//             child: Container(
+//               padding: EdgeInsets.all(10),
+//               margin: EdgeInsets.all(10),
+//               decoration: BoxDecoration(
+//                 border: Border.all(
+//                   color: Colors.blue,
+//                   width: 3,
+//                 ),
+//                 color: Colors.green,
+//                 borderRadius: BorderRadius.circular(20),
+//               ),
+//               width: 150,
+//               height: 150,
+//               child: Center(
+//                 child: Text(
+//                   "hey Flutter!",
+//                   textAlign: TextAlign.center,
+//                   style: TextStyle(
+//                     fontSize: 32,
+//                     color: Colors.black,
+//                     fontWeight: FontWeight.w700,
+//                     fontStyle: FontStyle.italic,
+//                     decoration: TextDecoration.underline,
+//                     decorationThickness: 2,
+//                     decorationColor: Colors.black,
+//                   ),
+//                 ),
+//               ),
+//             ),
+//           ),
+//           //! Red
+//           Positioned(
+//             left: 20,
+//             top: 100,
+//             child: Container(
+//               padding: EdgeInsets.all(10),
+//               margin: EdgeInsets.all(10),
+//               decoration: BoxDecoration(
+//                 border: Border.all(
+//                   color: Colors.blue,
+//                   width: 3,
+//                 ),
+//                 color: Colors.red,
+//                 borderRadius: BorderRadius.circular(20),
+//               ),
+//               width: 150,
+//               height: 150,
+//               child: Center(
+//                 child: Text(
+//                   "hey Flutter!",
+//                   textAlign: TextAlign.center,
+//                   style: TextStyle(
+//                     fontSize: 32,
+//                     color: Colors.black,
+//                     fontWeight: FontWeight.w700,
+//                     fontStyle: FontStyle.italic,
+//                     decoration: TextDecoration.underline,
+//                     decorationThickness: 2,
+//                     decorationColor: Colors.black,
+//                   ),
+//                 ),
+//               ),
+//             ),
+//           ),
+//           Positioned(
+//             left: 135,
+//             top: 120,
+//             child: Container(
+//               decoration:
+//                   BoxDecoration(color: Colors.blue, shape: BoxShape.circle),
+//               width: 30,
+//               height: 30,
+//               child: Center(
+//                 child: Icon(
+//                   Icons.add,
+//                   color: Colors.white,
+//                   size: 25,
+//                 ),
+//               ),
+//             ),
+//           ),
+//           // Yellow
+//           Positioned(
+//             right: 20,
+//             top: 300,
+//             child: Container(
+//               padding: EdgeInsets.all(10),
+//               margin: EdgeInsets.all(10),
+//               decoration: BoxDecoration(
+//                 border: Border.all(
+//                   color: Colors.blue,
+//                   width: 3,
+//                 ),
+//                 color: Colors.amber,
+//                 borderRadius: BorderRadius.circular(20),
+//               ),
+//               width: 150,
+//               height: 150,
+//               child: Center(
+//                 child: Text(
+//                   "hey Flutter!",
+//                   textAlign: TextAlign.center,
+//                   style: TextStyle(
+//                     fontSize: 32,
+//                     color: Colors.black,
+//                     fontWeight: FontWeight.w700,
+//                     fontStyle: FontStyle.italic,
+//                     decoration: TextDecoration.underline,
+//                     decorationThickness: 2,
+//                     decorationColor: Colors.black,
+//                   ),
+//                 ),
+//               ),
+//             ),
+//           ),
+//         ],
+//       ),
+//     ));
+//   }
+// }
+
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: SizedBox(
-      width: double.infinity,
+        body: SingleChildScrollView(
+      scrollDirection: Axis.vertical,
       child: Column(
+        spacing: 10,
         mainAxisAlignment: MainAxisAlignment.center,
         crossAxisAlignment: CrossAxisAlignment.center,
         children: [
-          //!1
+          Image.asset(ImageManager.flutterBird, width: 200, height: 200),
           Container(
-            padding: EdgeInsets.all(10),
-            margin: EdgeInsets.all(10),
-            decoration: BoxDecoration(
-              border: Border.all(
-                color: Colors.blue,
-                width: 3,
-              ),
-              color: Colors.red,
-              borderRadius: BorderRadius.circular(20),
-            ),
-            width: 150,
-            height: 150,
-            child: Center(
-              child: Text(
-                "Hello World!",
-                style: TextStyle(
-                  fontSize: 32,
-                  color: Colors.blue,
-                  fontWeight: FontWeight.w700,
-                  fontStyle: FontStyle.italic,
-                  decoration: TextDecoration.underline,
-                  decorationThickness: 2,
-                  decorationColor: Colors.blue,
+            color: Colors.amber,
+            height: 400,
+            width: 300,
+            child: Stack(
+              children: [
+                Positioned(
+                  top: 158,
+                  left: 13,
+                  child: Container(
+                    width: 250,
+                    height: 80,
+                    decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.circular(30)),
+                  ),
                 ),
-              ),
+                Positioned(
+                  top: 160,
+                  left: 10,
+                  child: Container(
+                    width: 250,
+                    height: 80,
+                    decoration: BoxDecoration(
+                        color: Colors.blue,
+                        borderRadius: BorderRadius.circular(30)),
+                  ),
+                ),
+                Positioned(
+                    top: 100,
+                    child: Image.asset(ImageManager.jetPack,
+                        width: 150, height: 150)),
+                Positioned(
+                  top: 190,
+                  right: 55,
+                  child: Text(
+                    "Cross Platform",
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 16,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+              ],
             ),
           ),
-          //! 2
+          Image.asset(ImageManager.android, width: 200, height: 200),
+          Image.asset(ImageManager.bloC, width: 200, height: 200),
+          Image.network(
+            ImageManager.flutterNetworkImage,
+          ),
           Container(
-            padding: EdgeInsets.all(10),
-            margin: EdgeInsets.all(10),
+            margin: EdgeInsets.all(20),
+            padding: EdgeInsets.all(20),
+            width: 200,
+            height: 200,
             decoration: BoxDecoration(
-              border: Border.all(
-                color: Colors.blue,
-                width: 3,
-              ),
-              color: Colors.amber,
-              borderRadius: BorderRadius.circular(20),
-            ),
-            width: 150,
-            height: 150,
-            child: Center(
-              child: Text(
-                "hey Flutter!",
-                style: TextStyle(
-                  fontSize: 32,
-                  color: Colors.black,
-                  fontWeight: FontWeight.w700,
-                  fontStyle: FontStyle.italic,
-                  decoration: TextDecoration.underline,
-                  decorationThickness: 2,
-                  decorationColor: Colors.black,
+              color: Colors.red,
+              borderRadius: BorderRadius.circular(30),
+              image: DecorationImage(
+                fit: BoxFit.fill,
+                image: NetworkImage(
+                  ImageManager.flutterBirdNetwork,
                 ),
               ),
             ),
+            child: Text(
+              "Hello",
+              textAlign: TextAlign.center,
+              style: TextStyle(color: Colors.white),
+            ),
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              //!1
+              Expanded(
+                flex: 1,
+                child: Container(
+                  padding: EdgeInsets.all(10),
+                  margin: EdgeInsets.all(10),
+                  decoration: BoxDecoration(
+                    border: Border.all(
+                      color: Colors.blue,
+                      width: 3,
+                    ),
+                    color: Colors.red,
+                    borderRadius: BorderRadius.circular(20),
+                  ),
+                  height: 150,
+                  child: Center(
+                    child: Visibility(
+                      visible: true,
+                      child: Text(
+                        "A",
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                          fontSize: 32,
+                          color: Colors.blue,
+                          fontWeight: FontWeight.w700,
+                          fontStyle: FontStyle.italic,
+                          decoration: TextDecoration.underline,
+                          decorationThickness: 2,
+                          decorationColor: Colors.blue,
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+              //! 2
+              Expanded(
+                flex: 2,
+                child: Container(
+                  padding: EdgeInsets.all(10),
+                  margin: EdgeInsets.all(10),
+                  decoration: BoxDecoration(
+                    border: Border.all(
+                      color: Colors.blue,
+                      width: 3,
+                    ),
+                    color: Colors.amber,
+                    borderRadius: BorderRadius.circular(20),
+                  ),
+                  height: 150,
+                  child: Center(
+                    child: Text(
+                      "B",
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                        fontSize: 32,
+                        color: Colors.black,
+                        fontWeight: FontWeight.w700,
+                        fontStyle: FontStyle.italic,
+                        decoration: TextDecoration.underline,
+                        decorationThickness: 2,
+                        decorationColor: Colors.black,
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            ],
           ),
         ],
       ),
@@ -92,12 +320,7 @@ class HomeScreen extends StatelessWidget {
   }
 }
 
-//TODO:  widgets => no child widget => text , one child widget , more one child (List<Widgets>) => Column , row
-
-//TODO:  widgets can be =>  text , image , audio , video , circle , every thing
-
-//! Task 
-//TODO:  MyApp , MaterialApp , Scaffold , Text , Center , Container , Align , Column , MainAxisAlignment. [(center=> (0,0)) (spaceAround 1 - 2 - 1)  (spaceEvenly 1 - 1 - 1)  (spaceBetween (center spcae))]  , Row , SizedBox
 
 
-//TODO:  naming in flutter => variables => homeScreen , class(Widgets) => HomeScreen  , files => home_screen
+//TODO:[Widgets] => Expanded (Column, Row) , Visibility , Wrap , Stack , Positioned , Icon
+//TODO:  Image.asset , Image.network , DecorationImage (Container) , ImageProvider 
